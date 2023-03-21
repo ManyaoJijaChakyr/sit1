fc5d359df8adc80e507874a277c4b3be81cc9f8b
ManyaoJijaChakyr <n.egorkin.64.2003@yandex.ru>
2023-03-21 20:04:14 +0300

    Change fib func to iterative

--- 4.py
+++ 4.py
@@ -1,4 +1,15 @@
 def fib(n):
-    if n == 1: return 0
-    if n == 2: return 1
-    else: return fib(n-1) + fib(n-2)
+    if n == 1:
+        print(0)
+    if n == 2:
+        print(0)
+        print(1)
+    else:
+        a, b = 0, 1
+        print(0)
+        print(1)
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+            print(c)
